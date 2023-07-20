34202828d87839d25242a29b313d7940e4c1b0bd
atmohamedamer <atmohamedamer@gmail.com>
2023-07-20 04:15:33 -0700

    0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import SelectLocale from "./components/SelectLocale/SelectLocale";
 
+import Onboarding from "./pages/Onboarding";
+import Splash from "./pages/Splash";
+import Unlock from "./pages/Unlock";
+
 export { 
     SelectLocale
 };
+
+export {
+    Onboarding,
+    Splash,
+    Unlock
+}

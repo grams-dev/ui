f0b9fbdd68193821d29fd4bab1b6dac40ba429a2
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2023-09-03 00:31:09 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,6 @@ export * from "./components/SelectItem";
 export * from "./components/SelectLocale";
 export * from "./components/SelectMethod";
 export * from "./components/SelectProfile";
+export * from "./components/Settings";
 export * from "./components/Unlock";
 export * from "./components/ViewSeed";
